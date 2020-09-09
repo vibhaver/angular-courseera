efe2a4c65ed1cfd73922cde1d0c422ea681c795e
[vibhaver] <[vibhaverma14.vv@gmail.com]>
2020-09-09 15:33:27 -0400

    component 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({                                 //it is a component decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
