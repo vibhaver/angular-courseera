4f2b8b6e2cc277773c5ed5fc7a4b4ad69d538a97
[vibhaver] <[vibhaverma14.vv@gmail.com]>
2020-09-25 13:41:22 -0400

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+
 import 'hammerjs';
 
 import { DishService } from './services/dish.service';
@@ -48,7 +53,11 @@ import { LoginComponent } from './login/login.component';
 		MatCardModule,
 		MatButtonModule,
 		AppRoutingModule,
-		MatDialogModule
+		MatDialogModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatCheckboxModule,
+		FormsModule
 
 	],
 	providers: [
